96990ee4d6ddc78988af09b9f00f7fdd538b5a4b
Ayomide Oluwatimilehin Ojudun <107155142+Oluwatimilehin2004@users.noreply.github.com>
2025-01-25 01:42:49 +0100

    Hey! Final Update

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
     EMAILJS_SERVICE_ID: "service_nr5cj9l",
-    EMAILJS_TEMPLATE_ID: "template_enm9zjd_id",
+    EMAILJS_TEMPLATE_ID: "template_enm9zjd",
     EMAILJS_PUBLIC_KEY: "nWxfLSB7whvOeMsZq",
   };
   
