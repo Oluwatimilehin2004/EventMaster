4196c54deb12850917477de73cafadf6ed859d80
Ayomide Oluwatimilehin Ojudun <107155142+Oluwatimilehin2004@users.noreply.github.com>
2025-01-25 02:21:59 +0100

    hiii

--- public/script.js
+++ public/script.js
@@ -140,7 +140,7 @@ let ticketId = Math.floor(10000 + Math.random() * 90000);
                 </section>
             </div>`;
 
-        generatePDF(); // Convert the current UI to a PDF and send it
+        // generatePDF(); // Convert the current UI to a PDF and send it
         sendEmailWithAttachment();
     });
 
